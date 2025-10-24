2ef70119b213710ba80f0d6bf2c41eeaba091030
Isabella Pompeo <162395260+Isabella-Pompeo@users.noreply.github.com>
2025-10-24 20:08:37 -0300

    atual01

---
+++ aula01.java
@@ -0,0 +1,10 @@
+package teste;
+public class primeiroCodigo {
+     public static void main (String (large)) {
+        int idade;
+        idade = 19;
+        String nome;
+        nome = "Fabricio Garcia"
+            System.out.printIn("O nome eh " + nome )
+     }
+}
